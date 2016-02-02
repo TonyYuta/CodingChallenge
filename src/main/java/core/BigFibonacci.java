6492ff837f21cbfa1f3048265ef151a468838af7
TonyYuta <tonykagaya@gmail.com>
2016-02-01 22:07:22 -0800

    BigFibonacci compleated

---
+++ src/main/java/core/BigFibonacci.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: BigFibonacci.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Feb 1, 2016
+ *   
+ */
+
+package core;
+
+import java.math.BigInteger;
+
+public class BigFibonacci {
+	
+	public static void main (String[] args) {
+		int max = 1000;
+        BigInteger[] fibonacci = new BigInteger[max];
+        fibonacci[0] = BigInteger.ZERO;
+        fibonacci[1] = BigInteger.ONE;
+        for (int i = 2; i < max; i++) {
+               fibonacci[i] = fibonacci[i - 1].add(fibonacci[i - 2]); 
+               }
+        for (int i = 0; i < max; i++) {
+        	//Reverse:  for (int i = fibonacci.length - 1; i >= 0; i--) {
+        System.out.println((i + 1) + ": " + fibonacci[i]); 
+        }
+	}
+}
